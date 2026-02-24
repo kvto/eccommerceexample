79886664eeaf64b2a07497ecb1603e4bbb654900
Kevin J. Monter Zea <kjms.15987@gmail.com>
2026-02-24 13:39:52 -0500

    fix: move inngest route inside app/api

--- inngest/functions.js
+++ inngest/functions.js
@@ -1,5 +1,5 @@
 import { inngest } from "./client";
-import prisma from "@/lib/db"
+import {prisma} from "@/lib/db"
 
 export const syncUserCreation = inngest.createFunction(
     {id: "sync-user-create"},
@@ -9,7 +9,7 @@ export const syncUserCreation = inngest.createFunction(
         await prisma.user.create({
             data: {
                 id: data.id,
-                email: data.email_addresses[0].email_addresses,
+                email: data.email_addresses[0].email_address,
                 name: `${data.first_name} ${data.last_name}`,
                 image: data.image_url,
             }
@@ -25,7 +25,7 @@ export const syncUserUpdation = inngest.createFunction(
         await prisma.user.update({
             where: {id: data.id},
             data: {
-                email: data.email_addresses[0].email_addresses,
+                email: data.email_addresses[0].email_address,
                 name: `${data.first_name} ${data.last_name}`,
                 image: data.image_url,
             }
